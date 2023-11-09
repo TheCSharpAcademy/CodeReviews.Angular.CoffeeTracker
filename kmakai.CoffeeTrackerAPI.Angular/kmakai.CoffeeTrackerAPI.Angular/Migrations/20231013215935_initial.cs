53dd5e1f9d5b10e2bb4f08f40b20105c37e51d57
kmakai <makaikwag@gmail.com>
2023-11-09 16:54:01 -0500

    submission for review

---
+++ kmakai.CoffeeTrackerAPI.Angular/kmakai.CoffeeTrackerAPI.Angular/Migrations/20231013215935_initial.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace kmakai.CoffeeTrackerAPI.Angular.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Coffees",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    cupSize = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Coffees", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Coffees");
+        }
+    }
+}
---
+++ kmakai.CoffeeTrackerAPI.Angular/kmakai.CoffeeTrackerAPI.Angular/Migrations/20231013221625_updatedCoffee2.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace kmakai.CoffeeTrackerAPI.Angular.Migrations
+{
+    /// <inheritdoc />
+    public partial class updatedCoffee2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CupSize",
+                table: "Coffees",
+                newName: "Cups");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Cups",
+                table: "Coffees",
+                newName: "CupSize");
+        }
+    }
+}
