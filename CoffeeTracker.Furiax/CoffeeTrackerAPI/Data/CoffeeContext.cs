31cdbbc41f87fbd941e4f1cfa2562352d8eb7ec0
furiax <carlmalfliet@proximus.be>
2024-01-18 11:20:49 +0100

    started on API created model, context and DI

---
+++ CoffeeTracker.Furiax/CoffeeTrackerAPI/Data/CoffeeContext.cs
@@ -0,0 +1,12 @@
+﻿using CoffeeTrackerAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoffeeTrackerAPI.Data
+{
+    public class CoffeeContext : DbContext
+    {
+        public CoffeeContext(DbContextOptions<CoffeeContext> options) : base(options) { }
+
+        public DbSet<Coffee> Coffees { get; set; } = null!;
+    }
+}
